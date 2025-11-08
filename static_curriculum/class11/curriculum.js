64f2d3114f2b07a20c3645f6a363f8d25b6a8dfd
ready4exam <ready4urexam@gmail.com>
2025-11-08 14:14:33 +0530

    Update curriculum.js

--- static_curriculum/class11/curriculum.js
+++ static_curriculum/class11/curriculum.js
@@ -1,1 +1,741 @@
-static_curriculum/class11/curriculum.js
+//static_curriculum/class11/curriculum.js
+export const curriculum = {
+  "Mathematics": {
+    "Mathematics (Part I & II)": [
+      {
+        "chapter_title": "Sets",
+        "table_id": "1",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Relations and Functions",
+        "table_id": "2",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Trigonometric Functions",
+        "table_id": "3",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Complex Numbers and Quadratic Equations",
+        "table_id": "4",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Linear Inequalities",
+        "table_id": "5",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Permutations and Combinations",
+        "table_id": "6",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Binomial Theorem",
+        "table_id": "7",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Sequence and Series",
+        "table_id": "8",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Straight Lines",
+        "table_id": "9",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Conic Sections",
+        "table_id": "10",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Introduction to Three Dimensional Geometry",
+        "table_id": "11",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Limits and Derivatives",
+        "table_id": "12",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Statistics",
+        "table_id": "13",
+        "section": "Science/Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Probability",
+        "table_id": "14",
+        "section": "Science/Commerce/Humanities"
+      }
+    ]
+  },
+  "Physics": {
+    "Physics Part I": [
+      {
+        "chapter_title": "Physical World",
+        "table_id": "1",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Units and Measurements",
+        "table_id": "2",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Motion in a Straight Line",
+        "table_id": "3",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Motion in a Plane",
+        "table_id": "4",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Laws of Motion",
+        "table_id": "5",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Work, Energy and Power",
+        "table_id": "6",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "System of Particles and Rotational Motion",
+        "table_id": "7",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Gravitation",
+        "table_id": "8",
+        "section": "Science"
+      }
+    ],
+    "Physics Part II": [
+      {
+        "chapter_title": "Mechanical Properties of Solids",
+        "table_id": "9",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Mechanical Properties of Fluids",
+        "table_id": "10",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Thermal Properties of Matter",
+        "table_id": "11",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Thermodynamics",
+        "table_id": "12",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Kinetic Theory",
+        "table_id": "13",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Oscillations",
+        "table_id": "14",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Waves",
+        "table_id": "15",
+        "section": "Science"
+      }
+    ]
+  },
+  "Chemistry": {
+    "Chemistry Part I": [
+      {
+        "chapter_title": "Some Basic Concepts of Chemistry",
+        "table_id": "1",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Structure of Atom",
+        "table_id": "2",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Classification of Elements and Periodicity in Properties",
+        "table_id": "3",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Chemical Bonding and Molecular Structure",
+        "table_id": "4",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Thermodynamics",
+        "table_id": "5",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Equilibrium",
+        "table_id": "6",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Redox Reactions",
+        "table_id": "7",
+        "section": "Science"
+      }
+    ],
+    "Chemistry Part II": [
+      {
+        "chapter_title": "Hydrogen",
+        "table_id": "8",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "The s-Block Elements",
+        "table_id": "9",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Some p-Block Elements",
+        "table_id": "10",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Organic Chemistry - Some Basic Principles and Techniques",
+        "table_id": "11",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Hydrocarbons",
+        "table_id": "12",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Environmental Chemistry",
+        "table_id": "13",
+        "section": "Science"
+      }
+    ]
+  },
+  "Biology": {
+    "Biology": [
+      {
+        "chapter_title": "The Living World",
+        "table_id": "1",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Biological Classification",
+        "table_id": "2",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Plant Kingdom",
+        "table_id": "3",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Animal Kingdom",
+        "table_id": "4",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Morphology of Flowering Plants",
+        "table_id": "5",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Anatomy of Flowering Plants",
+        "table_id": "6",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Structural Organisation in Animals",
+        "table_id": "7",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Cell: The Unit of Life",
+        "table_id": "8",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Biomolecules",
+        "table_id": "9",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Cell Cycle and Cell Division",
+        "table_id": "10",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Photosynthesis in Higher Plants",
+        "table_id": "11",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Respiration in Plants",
+        "table_id": "12",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Plant Growth and Development",
+        "table_id": "13",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Breathing and Exchange of Gases",
+        "table_id": "14",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Body Fluids and Circulation",
+        "table_id": "15",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Excretory Products and their Elimination",
+        "table_id": "16",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Locomotion and Movement",
+        "table_id": "17",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Neural Control and Coordination",
+        "table_id": "18",
+        "section": "Science"
+      },
+      {
+        "chapter_title": "Chemical Coordination and Integration",
+        "table_id": "19",
+        "section": "Science"
+      }
+    ]
+  },
+  "Accountancy": {
+    "Financial Accounting - I": [
+      {
+        "chapter_title": "Introduction to Accounting",
+        "table_id": "1",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Theory Base of Accounting",
+        "table_id": "2",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Recording of Transactions - I (Source Documents and Vouchers)",
+        "table_id": "3",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Recording of Transactions - II (Books of Original Entry - Journal)",
+        "table_id": "4",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Bank Reconciliation Statement",
+        "table_id": "5",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Trial Balance and Rectification of Errors",
+        "table_id": "6",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Depreciation, Provisions and Reserves",
+        "table_id": "7",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Financial Statements - I (Sole Proprietorship)",
+        "table_id": "8",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Financial Statements - II (Sole Proprietorship)",
+        "table_id": "9",
+        "section": "Commerce"
+      }
+    ]
+  },
+  "Business Studies": {
+    "Business Studies": [
+      {
+        "chapter_title": "Evolution and Fundamentals of Business",
+        "table_id": "1",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Forms of Business Organisations",
+        "table_id": "2",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Private, Public and Global Enterprises",
+        "table_id": "3",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Business Services",
+        "table_id": "4",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Emerging Modes of Business",
+        "table_id": "5",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Social Responsibility of Business and Business Ethics",
+        "table_id": "6",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Sources of Business Finance",
+        "table_id": "7",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Small Business",
+        "table_id": "8",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "Internal Trade",
+        "table_id": "9",
+        "section": "Commerce"
+      },
+      {
+        "chapter_title": "International Business",
+        "table_id": "10",
+        "section": "Commerce"
+      }
+    ]
+  },
+  "Economics": {
+    "Statistics for Economics": [
+      {
+        "chapter_title": "Introduction",
+        "table_id": "1",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Collection of Data",
+        "table_id": "2",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Organisation of Data",
+        "table_id": "3",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Presentation of Data",
+        "table_id": "4",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Measures of Central Tendency",
+        "table_id": "5",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Measures of Dispersion",
+        "table_id": "6",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Correlation",
+        "table_id": "7",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Index Numbers",
+        "table_id": "8",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Use of Statistical Tools",
+        "table_id": "9",
+        "section": "Commerce/Humanities"
+      }
+    ],
+    "Indian Economic Development": [
+      {
+        "chapter_title": "Indian Economy on the Eve of Independence",
+        "table_id": "10",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Indian Economy (1950-1990)",
+        "table_id": "11",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Liberalisation, Privatisation and Globalisation: An Appraisal",
+        "table_id": "12",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Poverty",
+        "table_id": "13",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Human Capital Formation in India",
+        "table_id": "14",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Rural Development",
+        "table_id": "15",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Employment: Growth, Informalisation and Other Issues",
+        "table_id": "16",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Infrastructure",
+        "table_id": "17",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Environment and Sustainable Development",
+        "table_id": "18",
+        "section": "Commerce/Humanities"
+      },
+      {
+        "chapter_title": "Comparative Development Experiences of India and its Neighbours",
+        "table_id": "19",
+        "section": "Commerce/Humanities"
+      }
+    ]
+  },
+  "History": {
+    "Themes in World History": [
+      {
+        "chapter_title": "Introduction to World History",
+        "table_id": "1",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Early Societies",
+        "table_id": "2",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Empires",
+        "table_id": "3",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Changing Traditions",
+        "table_id": "4",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Paths to Modernisation",
+        "table_id": "5",
+        "section": "Humanities"
+      }
+    ]
+  },
+  "Political Science": {
+    "Indian Constitution at Work": [
+      {
+        "chapter_title": "Constitution: Why and How?",
+        "table_id": "1",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Rights in the Indian Constitution",
+        "table_id": "2",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Election and Representation",
+        "table_id": "3",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Executive",
+        "table_id": "4",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Legislature",
+        "table_id": "5",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Judiciary",
+        "table_id": "6",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Federalism",
+        "table_id": "7",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Local Governments",
+        "table_id": "8",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Constitution as a Living Document",
+        "table_id": "9",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "The Philosophy of the Constitution",
+        "table_id": "10",
+        "section": "Humanities"
+      }
+    ],
+    "Political Theory": [
+      {
+        "chapter_title": "Political Theory: An Introduction",
+        "table_id": "11",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Freedom",
+        "table_id": "12",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Equality",
+        "table_id": "13",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Social Justice",
+        "table_id": "14",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Rights",
+        "table_id": "15",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Citizenship",
+        "table_id": "16",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Nationalism",
+        "table_id": "17",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Secularism",
+        "table_id": "18",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Peace",
+        "table_id": "19",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Development",
+        "table_id": "20",
+        "section": "Humanities"
+      }
+    ]
+  },
+  "Geography": {
+    "Fundamentals of Physical Geography": [
+      {
+        "chapter_title": "Geography as a Discipline",
+        "table_id": "1",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "The Earth",
+        "table_id": "2",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Distribution of Oceans and Continents",
+        "table_id": "3",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Minerals and Rocks",
+        "table_id": "4",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Geomorphic Processes",
+        "table_id": "5",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Landforms and their Evolution",
+        "table_id": "6",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Composition and Structure of Atmosphere",
+        "table_id": "7",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Solar Radiation, Heat Balance and Temperature",
+        "table_id": "8",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Atmospheric Circulation and Weather Systems",
+        "table_id": "9",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Water in the Atmosphere",
+        "table_id": "10",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "World Climate and Global Warming",
+        "table_id": "11",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Water (Oceans)",
+        "table_id": "12",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Life on the Earth",
+        "table_id": "13",
+        "section": "Humanities"
+      },
+      {
+        "chapter_title": "Biodiversity and Conservation",
+        "table_id": "14",
+        "section": "Humanities"
+      }
+    ]
+  }
+}
