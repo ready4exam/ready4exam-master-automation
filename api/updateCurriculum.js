871c8197b68c76eb00145712ae580534f9ba2a3b
ready4exam <ready4urexam@gmail.com>
2025-11-08 08:12:02 +0530

    Update updateCurriculum.js

--- api/updateCurriculum.js
+++ api/updateCurriculum.js
@@ -1,4 +1,4 @@
-// -------------------- /api/updateCurriculum.js --------------------
+// -------------------- /api/updateCurriculum.js -------------------
 import { corsHeaders } from "./_cors.js";
 
 export const config = { runtime: "nodejs" };
