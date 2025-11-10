a07b58296d87527c4eea03d003d7f1b4961b5c9b
ready4exam <ready4urexam@gmail.com>
2025-11-10 15:48:18 +0530

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, message: "✅ API route deployed successfully" });
+  res.status(200).json({ ok: true, message: "✅ API route deployed successfully" }); 
 }
